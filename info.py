a14783fd6ca77a774ff4068db81461385327e283
Emre Cenk <62028675+EmreCenk@users.noreply.github.com>
2021-02-23 22:38:49 -0500

    Create info.py

---
+++ info.py
@@ -0,0 +1,7 @@
+
+# Username and password do not have to belong to the account you are trying to analyze. 
+# As long as the given account follows the account, you can still extract data. 
+# For instance, you could use an alt account to analyze your own account (if you do not want to save your own password on a computer).
+
+username="Enter a username to sign in"
+password="Enter the corresponding password"
